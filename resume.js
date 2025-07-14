9f8915caee1ccdcac87264485dfbdc58c25c88ea
Idham Malik <idham1432@gmail.com>
2025-07-15 03:27:14 +0800

    updated resume file

--- resume.js
+++ resume.js
@@ -1,13 +1,13 @@
 document.getElementById('btn-resume').addEventListener('click', function () {
   const link = document.createElement('a');
-  link.href = 'Resume.pdf'; // Assuming Resume.pdf is in the root folder
-  link.download = 'Resume.pdf'; // Optional: Rename file on download
+  link.href = 'Resume - Idham Malik.pdf';
+  link.download = 'Resume - Idham Malik.pdf';
   link.click();
 });
 
 document.getElementById('btn-resume-sidebar').addEventListener('click', function () {
   const link = document.createElement('a');
-  link.href = 'Resume.pdf'; // Assuming Resume.pdf is in the root folder
-  link.download = 'Resume.pdf'; // Optional: Rename file on download
+  link.href = 'Resume - Idham Malik.pdf';
+  link.download = 'Resume - Idham Malik.pdf';
   link.click();
 });

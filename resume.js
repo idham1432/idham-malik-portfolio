b2dab95f18add1c4578d637373284bdec605b681
Idham Malik <idham1432@gmail.com>
2025-07-14 18:24:31 +0800

    get resume button

--- resume.js
+++ resume.js
@@ -4,3 +4,10 @@ document.getElementById('btn-resume').addEventListener('click', function () {
   link.download = 'Resume.pdf'; // Optional: Rename file on download
   link.click();
 });
+
+document.getElementById('btn-resume-sidebar').addEventListener('click', function () {
+  const link = document.createElement('a');
+  link.href = 'Resume.pdf'; // Assuming Resume.pdf is in the root folder
+  link.download = 'Resume.pdf'; // Optional: Rename file on download
+  link.click();
+});

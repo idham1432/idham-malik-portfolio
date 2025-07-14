91446e28605f80ecbbdee29689ecc4e336271d31
Idham Malik <idham1432@gmail.com>
2025-07-14 15:32:44 +0800

    download resume

---
+++ resume.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-resume').addEventListener('click', function () {
+  const link = document.createElement('a');
+  link.href = 'Resume.pdf'; // Assuming Resume.pdf is in the root folder
+  link.download = 'Resume.pdf'; // Optional: Rename file on download
+  link.click();
+});
